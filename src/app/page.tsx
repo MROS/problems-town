4c24606dba5e4df5e13e401e08de1cf9d04d24b1
MROS <yc1043@gmail.com>
2024-01-28 01:36:34 +0800

    採納 create-t3-app 更新後的模板，解決 pnpm build 報錯問題

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Link from "next/link";
+import { unstable_noStore as noStore } from "next/cache";
 
 import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
+  noStore();
   const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
