2c938bbe61898264a53426ef8b92a97fab68b738
MROS <yc1043@gmail.com>
2024-03-10 00:22:06 +0800

    完成答案卡片雛形

--- src/app/book/id/[id]/[chapterId]/exercise/[exerciseId]/answer/answerCard.tsx
+++ src/app/book/id/[id]/[chapterId]/exercise/[exerciseId]/answer/answerCard.tsx
@@ -1,16 +1,63 @@
 "use client";
 import { type User, type Answer } from "@prisma/client";
 import { MyMarkdown } from "~/app/_components/myMarkdown";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/react";
+import { IoMdMore } from "react-icons/io";
 
 type AnswerWithAuthor = Answer & { author: User };
 
 export default function AnswerCard(props: { answer: AnswerWithAuthor }) {
   const { answer } = props;
   return (
-    <div>
-      <div>{answer.author.name}</div>
-      <div>{answer.createDate.toDateString()}</div>
-      <MyMarkdown>{answer.text}</MyMarkdown>
-    </div>
+    <Card>
+      <CardHeader>
+        <div className="flex w-full flex-row justify-between">
+          <div>
+            <div>{answer.author.name}</div>
+            <div className="text-small text-gray-500">
+              {/* // TODO: 輸出中文相對日期 */}
+              {answer.createDate.toDateString()}
+            </div>
+          </div>
+          <div>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button isIconOnly variant="light">
+                  <IoMdMore className="text-xl" />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu disabledKeys={["edit", "copy", "enter"]}>
+                <DropdownItem key="edit">編輯</DropdownItem>
+                <DropdownItem key="copy">複製網址</DropdownItem>
+                <DropdownItem key="enter">進入答案頁</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </div>
+      </CardHeader>
+      <CardBody className="ppb-3">
+        <MyMarkdown>{answer.text}</MyMarkdown>
+      </CardBody>
+      <CardFooter>
+        <div className="grid w-full grid-cols-2 space-x-2">
+          <Button variant="bordered" size="sm">
+            批改
+          </Button>
+          <Button variant="bordered" size="sm">
+            評論
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
   );
 }
