afa42dd23c039b39ccf356a4c0d9109778e0cfae
MROS <yc1043@gmail.com>
2024-04-04 19:58:51 +0800

    顯示所有活動

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -1,10 +1,30 @@
+import { type Activity } from "@prisma/client";
 import NewActivityButton from "./newActivityButton";
+import { db } from "~/server/db";
+import { Link } from "@nextui-org/react";
 
 export const metadata = {
   title: "活動 | 做題小鎮",
 };
 
+function ShowActivities({ activities }: { activities: Activity[] }) {
+  // TODO: 顯示活動的更多資訊
+  return (
+    <div>
+      {activities.map((activity) => {
+        return (
+          <div key={activity.id}>
+            <Link href={`/activity/${activity.id}`}>{activity.name}</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
 export default async function Activity() {
+  // TODO: 僅顯示近期的活動
+  const activities = await db.activity.findMany({});
   return (
     <main className="flex w-screen grow flex-col items-center pt-10">
       <div className="flex w-full max-w-xl flex-col px-2">
@@ -12,6 +32,7 @@ export default async function Activity() {
           <div className="font-bold">活動</div>
           <NewActivityButton />
         </div>
+        <ShowActivities activities={activities} />
       </div>
     </main>
   );
